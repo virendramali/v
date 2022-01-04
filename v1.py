75acd9aa3e6b383908dca786ac0d05bec6263fc1
virendramali <virendravmali77@gmail.com>
2022-01-04 21:56:58 +0530

    some changes

--- v1.py
+++ v1.py
@@ -1,1 +1,10 @@
-print("v1.py file")
+# Program to move all 0's at starting and
+# remaining elements at last
+a = [1, 0, 3, 3, 5, 0, 0, 4]
+l = []
+for i in a:
+    if i == 0:
+        l.insert(0, i)
+    else:
+        l.append(i)
+print(l)
